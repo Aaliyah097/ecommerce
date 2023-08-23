20a4831b3fa3b04f1092c9d9ecd1b1bbdb3d8cc9
Aaliyah097 <name.boltz@gmail.com>
2023-08-23 18:20:21 +0300

    json dumps categories

--- catalog/views.py
+++ catalog/views.py
@@ -13,5 +13,5 @@ def categories(request):
 
     return HttpResponse(
         status=status.HTTP_200_OK,
-        content=repo.serialize(repo.list())
+        content=json.dumps(repo.serialize(repo.list()))
     )
