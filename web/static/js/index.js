2c40a3f2ff05065bf8c2a75a52d487b6d74642b1
eldenhard <elden.hard@mail.ru>
2023-08-29 15:13:41 +0300

    dashboard

---
+++ web/static/js/index.js
@@ -0,0 +1,83 @@
+
+
+// Перебор всех категорий
+
+// function printNames(data, arr_categories = []) {
+//     for (let item of data) {
+//         if (!arr_categories.includes(item.name)) {
+//             arr_categories.push(item.name);
+//         }
+//         if (item.children) {
+//             printNames(item.children, arr_categories);
+//         }
+//     }
+//     return arr_categories;
+// }
+
+
+let tree = document.getElementById('tree')
+let refreshData = document.getElementById("refreshData")
+
+function renderList(item, level = 0) {
+    const listItem = document.createElement("li");
+    const spanItem = document.createElement("span");
+    spanItem.textContent = item.name;
+
+    listItem.appendChild(spanItem);
+
+    if (item.children && item.children.length > 0) {
+        const sublist = document.createElement("ul");
+        listItem.appendChild(sublist);
+        for (const child of item.children) {
+            const subListItem = renderList(child, level + 1);
+            sublist.appendChild(subListItem);
+        }
+    }
+
+    return listItem;
+}
+
+function get_categories() {
+    let csrf_token = $('input[name="csrfmiddlewaretoken"]').val();
+    $.ajax(
+        {
+            type: 'get',
+            url: '/catalog/categories',
+            headers: { 'X-CSRFToken': csrf_token },
+            mode: 'same-origin',
+            async: true,
+            success: function (json) {
+                data = JSON.parse(json)
+
+                for (const item of data) {
+                    const listItem = renderList(item);
+                    tree.appendChild(listItem);
+                }
+                
+                new TreeDragZone(tree);
+                new TreeDropTarget(tree);
+            },
+            error: function (xhr, errmsg, err) {
+                console.log(errmsg);
+            }
+        }
+    )
+}
+
+refreshData.addEventListener('click', () => {
+    refreshData.classList.toggle('transform');
+    setTimeout(() => {
+        tree.innerHTML = ""
+        get_categories();
+    }, 800)
+    tree.innerHTML = ""
+    get_categories();
+})
+
+
+
+
+
+
+
+
