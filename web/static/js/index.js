38c75907472d4d5e7b879a88093adeb8a317bdba
Aaliyah097 <name.boltz@gmail.com>
2023-09-13 09:43:51 +0300

    mobile adaptive

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -43,6 +43,60 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    let links = document.getElementsByName("page-link");
+
+    let collapse_class = "ant-menu-overflow-item ant-menu-item ant-menu-item-selected ant-menu-item-only-child";
+    let collapse_style = "opacity: 0; order: 0; height: 0px; overflow-y: hidden; pointer-events: none; position: absolute;";
+
+    if (window.screen.width <= 900){
+        for (let i = 0; i < links.length; i++){
+            links[i].setAttribute('class', collapse_class);
+            links[i].setAttribute('style', collapse_style);
+        }
+        document.getElementById('menu_links').insertAdjacentHTML('beforeend', `
+            <li id="menu_links_list" class="ant-menu-overflow-item ant-menu-overflow-item-rest ant-menu-submenu ant-menu-submenu-horizontal ant-menu-submenu-selected" style="opacity: 1; order: -1; vertical-align: middle;" role="none">
+                <div id="menu_links_div" role="menuitem" class="ant-menu-submenu-title" tabindex="-1" aria-expanded="false" aria-haspopup="true" style="vertical-align: middle; padding-top: 150%">
+                    <span role="img" aria-label="ellipsis" class="anticon anticon-ellipsis">
+                        <svg viewBox="64 64 896 896" focusable="false" data-icon="ellipsis" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+                            <path d="M176 511a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0zm280 0a56 56 0 10112 0 56 56 0 10-112 0z"></path>
+                        </svg>
+                    </span>
+                <i class="ant-menu-submenu-arrow"></i>
+            </div>
+        </li>`)
+    }
+
+    if (window.screen.width <= 900){
+        let filter_block = document.getElementById("filter-list-block");
+        let products_block = document.getElementById("products-list-block");
+        let new_filter_block = document.getElementById("new-filter-block");
+
+        filter_block.remove();
+        products_block.setAttribute('class', 'ant-col ant-col-24');
+
+        filter_block.setAttribute('class', 'ant-col ant-col-24');
+        new_filter_block.appendChild(filter_block);
+
+        let elements = document.getElementsByClassName('ant-menu-submenu-title');
+        for (let i = 0; i < elements.length; i++){
+            let parent = elements[i].parentNode;
+            let child = parent.querySelector('ul');
+            if (child){
+                parent.classList.toggle('ant-menu-submenu-open');
+                child.classList.toggle('ant-menu-hidden');
+            }
+        }
+    }
+})
+
+document.getElementById("menu_links").addEventListener('click', function(){
+    if (document.getElementById("menu_links_list")) {
+        let sticky_menu = document.getElementById("sticky_menu");
+        sticky_menu.classList.toggle('more_info_hidden');
+    }
+})
+
 function color_menu_links(){
     let links = document.getElementsByName("page-link");
     let path = window.location.href.split('/');
